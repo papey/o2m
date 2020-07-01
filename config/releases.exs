0cbabfec7079a108179ab7f689a7f5471c7faa27
Wilfried OLLIVIER <wollivier@fdn.fr>
2020-07-01 17:21:54 +0200

    Bump version to 0.3.4

--- config/releases.exs
+++ config/releases.exs
@@ -9,14 +9,16 @@ config :nostrum,
 
 # o2m configuration
 config :o2m,
-  # Username on Discord
-  username: "Orgie 2 Metal",
+  # Nickname on Discord
+  nickname: System.get_env("O2M_NICKNAME", "Orgie 2 Metal"),
   # Prefix used for commands
   prefix: System.get_env("O2M_PREFIX", "!"),
   # Metalorgie url, used for `band` and `album` command
   metalorgie: "https://www.metalorgie.com",
   # Time between each check on feeds
   timer: System.get_env("O2M_JOBS_TIMER", "500"),
+  # Guild ID
+  guild: System.get_env("O2M_GUILD_ID"),
   # Channel ID
   chan: System.get_env("O2M_CHAN_ID"),
   # RSS podcast feed urls
