4af9ba8b1c94a652124ca5e20b53878bee2ac76b
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-02-09 20:57:32 +0100

    Fix: refactor colors

--- lib/blindtest/colors.ex
+++ lib/blindtest/colors.ex
@@ -3,18 +3,19 @@ defmodule Colors do
   Fetch colors for various embed Discord message
   """
 
+  @colors %{
+    :info => 39372,
+    :success => 51281,
+    :warning => 16_746_496,
+    :danger => 13_369_344
+  }
+
   @doc """
   Map colors to atoms
 
   Return decimal color associated
   """
   def get_color(kind) do
-    case kind do
-      :info -> 39372
-      :success -> 51281
-      :warning -> 16_746_496
-      :danger -> 13_369_344
-      _ -> 0
-    end
+    Map.get(@colors, kind, 0)
   end
 end
