3bc586f91eb78e3f4f476e48935838209afb5c74
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-06-02 22:02:48 +0200

    Bump version to 0.10.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule O2M.MixProject do
   def project do
     [
       app: :o2m,
-      version: "0.10.1",
+      version: "0.10.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
