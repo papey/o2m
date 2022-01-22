a97df4aca538f3df1501c0753cf1872c23b4ddd5
Wilfried OLLIVIER <wollivier@fdn.fr>
2022-01-22 13:27:18 +0100

    More deps update

--- mix.exs
+++ mix.exs
@@ -30,9 +30,9 @@ defmodule O2M.MixProject do
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:nostrum, "~> 0.4"},
       {:elixir_feed_parser, "~> 0.0.1"},
-      {:timex, "~> 3.5"},
-      {:jason, "~> 1.1"},
-      {:tesla, "~> 1.3.0"},
+      {:timex, "~> 3.7"},
+      {:jason, "~> 1.3"},
+      {:tesla, "~> 1.4"},
       {:gen_state_machine, "~> 3.0.0"}
     ]
   end
