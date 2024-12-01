c484cf1f6917f6839c6df36dbd1ff95f9b4fb1d3
Wilfried OLLIVIER <wollivier@fdn.fr>
2024-12-01 21:00:14 +0100

    fix: exclude some test from CI because of Cloudflare

--- test/metalorgie_test.exs
+++ test/metalorgie_test.exs
@@ -2,21 +2,25 @@ defmodule MetalorgieTest do
   use ExUnit.Case
   doctest Metalorgie
 
+  @tag :no_ci
   test "get_band for band `korn`" do
     {:ok, %{url: url}} = Metalorgie.get_band(["korn"])
     assert url == "http://www.metalorgie.com/groupe/Korn"
   end
 
+  @tag :no_ci
   test "get_band for band `opeth`" do
     {:ok, result} = Metalorgie.get_band(["opeth"])
     assert result[:desc] == "Les plus atypiques du death suédois"
   end
 
+  @tag :no_ci
   test "get_band for band `nopnop`" do
     {:error, message} = Metalorgie.get_band(["nopnop"])
     assert message == "No band with name **nopnop** found"
   end
 
+  @tag :no_ci
   test "get_albums for band `iron maiden`" do
     {:ok, result} = Metalorgie.get_albums(["iron", "maiden"])
     assert length(result) >= 10
