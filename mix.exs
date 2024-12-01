37fd249874294098ca9a19954aa1b93d5b570d36
Wilfried OLLIVIER <wollivier@fdn.fr>
2024-12-01 21:08:10 +0100

    chore: bump version to 0.14.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule O2M.MixProject do
   def project do
     [
       app: :o2m,
-      version: "0.13.3",
+      version: "0.14.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
