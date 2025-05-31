6b0a0a53e2035324e1d9d06257d5d43305811292
Wilfried OLLIVIER <wollivier@fdn.fr>
2025-05-31 16:02:03 +0200

    chore: elixir 1.18

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule O2M.MixProject do
     [
       app: :o2m,
       version: "0.14.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: [
